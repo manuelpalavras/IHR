9890cb9810ea78e2b3d21c5ca0f9080baceec2ec
manuelpalavras <manuelpalavras19@gmail.com>
2018-12-13 10:44:33 +0000

    pesquisa melhorada

--- app/express/public/js/global.js
+++ app/express/public/js/global.js
@@ -1,4 +1,3 @@
-
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(showPosition);
@@ -9,9 +8,26 @@ function getLocation() {
 }
 
 function showPosition(position) {
-    $.post('/coordinates', position.coords , null, 'coordinates');
+    $.post('/coordinates', position.coords, null, 'coordinates');
 }
 
 function search() {
-    $("#button").attr('href', `/city=${$('#search').val()}`)
+    $("#button").attr('href', `/city=${capitalizeFirstLetter($('#search').val().toLowerCase())}`)
 }
+
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+let input = document.getElementById("search");
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function (event) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Trigger the button element with a click
+        document.getElementById("button").click();
+    }
+});
